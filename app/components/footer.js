de0490d1ebc7e3a45a28fd25c2ebdd8253af6318
johdirr <johannesdirr@me.com>
2016-07-03 18:55:06 +0200

    minor design fix

--- app/components/footer.js
+++ app/components/footer.js
@@ -4,17 +4,13 @@ import { Link } from 'react-router';
 
 const Footer = () => (
   <footer>
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-12 text-center links">
-          <Link to="/impressum">Impressum</Link>-
-          <a href="http://benjaminfriedl.de" target="blank">Benjamin</a>-
-          <a href="http://joh.berlin" target="blank">Johannes</a>-
-          <a href="mailto:mail@hauptsache-serioes.de">
-            mail@hauptsache-serioes.de
-          </a>
-        </div>
-      </div>
+    <div className="text-center links">
+      <Link to="/impressum">Impressum</Link>-
+      <a href="http://benjaminfriedl.de" target="blank">Benjamin</a>-
+      <a href="http://joh.berlin" target="blank">Johannes</a>-
+      <a href="mailto:mail@hauptsache-serioes.de">
+        mail@hauptsache-serioes.de
+      </a>
     </div>
   </footer>
 );
