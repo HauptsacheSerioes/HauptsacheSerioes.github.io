f67f1342a3c9b3928e5c6dd968b79d456a7f66ea
johdirr <johannesdirr@me.com>
2016-07-29 17:23:23 +0200

    changed links

--- app/components/footer.js
+++ app/components/footer.js
@@ -6,8 +6,8 @@ const Footer = () => (
   <footer>
     <div className="text-center links">
       <Link to="/impressum">Impressum</Link>-
-      <a href="http://benjaminfriedl.de" target="blank">Benjamin</a>-
-      <a href="http://joh.berlin" target="blank">Johannes</a>-
+      <a href="http://benjamin.hauptsache-serioes.de" target="blank">Benjamin</a>-
+      <a href="https://johannes.hauptsache-serioes.de" target="blank">Johannes</a>-
       <a href="mailto:mail@hauptsache-serioes.de">
         mail@hauptsache-serioes.de
       </a>
